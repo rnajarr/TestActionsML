e0b19537b379a299857c7bef9b725f2d408e8bbd
rnajarr <148824418+rnajarr@users.noreply.github.com>
2023-10-26 10:33:50 -0600

    Create example.m

---
+++ scripts/example.m
@@ -0,0 +1,8 @@
+% Calcula la suma de dos números y muestra el resultado por pantalla
+
+num1 = 5;
+num2 = 10;
+suma = num1 + num2;
+disp(['La suma de ', num2str(num1), ' y ', num2str(num2), ' es ', num2str(suma)]);
+
+
